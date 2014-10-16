b61c4483fe37a11ea333e9c1a07903fc162b69da
Meticulus <theonejohnnyd@gmail.com>
2014-10-16 07:45:57 +0530

    OMX : Usebuffer optimization.
    -The ifdef'd out code calls into our blobs to see if we support
    useAndroidNativeBuffer2 which we do not then it will fall back to
    useAndroidNativeBuffer. This patch just skips the check goes
    immediately to the fall back. Might save a little time.

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -557,6 +557,13 @@ status_t OMXNodeInstance::useGraphicBuffer(
     Mutex::Autolock autoLock(mLock);
 
     // See if the newer version of the extension is present.
+#ifndef STE_HARDWARE
+    /* Meticulus:
+     * We don't support useAndroidNativeBuffer2 and although it works fine
+     * to have this in, we are always going to fall back
+     * to useAndroidNativeBuffer everytime. Removing this might speed things
+     * up a little bit.
+     */
     OMX_INDEXTYPE index;
     if (OMX_GetExtensionIndex(
             mHandle,
@@ -564,7 +571,7 @@ status_t OMXNodeInstance::useGraphicBuffer(
             &index) == OMX_ErrorNone) {
         return useGraphicBuffer2_l(portIndex, graphicBuffer, buffer);
     }
-
+#endif
     OMX_STRING name = const_cast<OMX_STRING>(
         "OMX.google.android.index.useAndroidNativeBuffer");
     OMX_ERRORTYPE err = OMX_GetExtensionIndex(mHandle, name, &index);
